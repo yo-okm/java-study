e83613507ddc4fa58c17ec4bb69f3eb1be7249cd
ykm <ykm5708@gmail.com>
2023-02-18 14:41:55 +0900

    Section 8. Bean Lifecycle Callback (Annotation @PostConstruct, @PreDestroy)

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -15,7 +15,7 @@ public class BeanLifeCycleTest {
     }
 
     static class LifeCycleConfig {
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
